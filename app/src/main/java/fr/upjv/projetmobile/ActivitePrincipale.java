7b37af2316ead1d5de869947938b3042045a16fd
Gabriel Hardy <60385281+HAPPY-STEVE@users.noreply.github.com>
2022-05-03 18:00:45 +0200

    Squelette layout Lister

--- app/src/main/java/fr/upjv/projetmobile/ActivitePrincipale.java
+++ app/src/main/java/fr/upjv/projetmobile/ActivitePrincipale.java
@@ -3,6 +3,7 @@ package fr.upjv.projetmobile;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class ActivitePrincipale extends AppCompatActivity {
 
@@ -10,5 +11,17 @@ public class ActivitePrincipale extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_activite_principale);
+    }
+
+    public void onClickQuitterActivitePrincipale(View view) {
+    }
+
+    public void onClickEnregistrerActivitePrincipale(View view) {
+    }
+
+    public void onClickListerActivitePrincipale(View view) {
+    }
+
+    public void onClickSaisirActivitePrincipale(View view) {
     }
 }
--- app/src/main/java/fr/upjv/projetmobile/ListeLivre.java
+++ app/src/main/java/fr/upjv/projetmobile/ListeLivre.java
@@ -3,6 +3,7 @@ package fr.upjv.projetmobile;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class ListeLivre extends AppCompatActivity {
 
@@ -10,5 +11,8 @@ public class ListeLivre extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_activite_liste_livre);
+    }
+
+    public void onClickRetourLister(View view) {
     }
 }
